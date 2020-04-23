4c464a9d0c7c73e786eaf2df06d6bcb20ed8feea
AngelGR3 <angelguardador3@gmail.com>
2020-04-23 12:01:54 -0700

    initial

---
+++ proyecto-personas/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Route, Router } from 'react-router'
+import PropTypes from 'prop-types'
+import './App.css';
+
+import Home from './views/Home'
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={ this.props.history }>
+        <div className="App">
+          <Route exact path="/" component={ Home }/>
+        
+        </div>
+      </Router>
+    );
+  }
+}
+
+App.propTypes = {
+  history: PropTypes.any.isRequired
+}
+
+export default App;
---
+++ proyecto-personas/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import { getUsers, getUserById } from './UserReducer'
+
+
+export default combineReducers({
+  getUsers,
+  getUserById
+});
